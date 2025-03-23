bee6fe645f8417f6bf16c69fbb41312a03e00c48
Shirinkin Denis <shirinkin.denis@bk.ru>
2025-03-23 22:56:43 +0500

    Базовый путь для github-pages

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,11 @@ import vue from '@vitejs/plugin-vue'
 import {resolve} from 'path'
 import VitePluginVueDevTools from 'vite-plugin-vue-devtools'
 import { createHtmlPlugin } from 'vite-plugin-html'
+import { env } from 'process'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: env['url'] || '/',
   plugins: [
     vue(),
 
